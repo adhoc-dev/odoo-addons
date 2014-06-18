01e627bb94f96612827c1e51dca86326658f4885
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-06-18 20:03:29 +0200

    rm opnerpl-n_ar

--- openerp-l10n-ar-localization/l10n_ar_account_check_duo/wizard_third/__init__.py
+++
@@ -1,5 +0,0 @@
-import check_deposit
-import check_hreject
-import check_dreject
-import check_sold
-import ticket_check_deposit
--- openerp-l10n-ar-localization/l10n_ar_receipt/__init__.py
+++
@@ -1,6 +0,0 @@
-import receipt
-import account_voucher
-import receiptbook
-import receipt_pay
-import report
-
