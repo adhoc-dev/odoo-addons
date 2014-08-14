610799bd919beef2a46f5fbcd6f79d7cef3d54ef
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-08-14 17:58:19 -0300

    ADD availability

---
+++ sale_stock_availability/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Stock information in sale order line',
+    'version': '0.1',
+    'category': 'Tools',
+    'description': "",
+    'author': 'Moldeo Interactive & Ingenieria Adhoc',
+    'website': 'http://business.moldeo.coop http://ingadhoc.com/',
+    'images': [],
+    'depends': ['sale_stock'],
+    'demo': [],
+    'data': [
+        'sale_view.xml',
+        'security/sale_order.xml',
+        ],
+    'test': [],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_stock_availability/sale_order.py
@@ -0,0 +1,29 @@
+# -*- coding    : utf-8 -*-
+from openerp import fields, models, api
+
+
+class sale_order_line_stock(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.one
+    @api.depends(
+        'product_uom_qty',
+        'product_id')
+    def _fnct_line_stock(self):
+        available = False
+        print 'context',  self.env.context
+        if self.order_id.state == 'draft':
+            available = self.with_context(
+                warehouse=self.order_id.warehouse_id.id
+                ).product_id.virtual_available - self.product_uom_qty
+        self.virtual_available = available
+        if available >= 0.0:
+            available = True
+        self.virtual_available_boolean = available
+
+    virtual_available = fields.Float(
+        compute="_fnct_line_stock", string='Saldo Stock')
+    virtual_available_boolean = fields.Boolean(
+        compute="_fnct_line_stock", string='Saldo Stock')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
