68e0f00448f0a7b25e7c49c8446208853fbaa4b7
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-08-21 18:37:47 -0300

    ADD account voucher account fix

---
+++ account_voucher_account_fix/account_voucher.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv
+
+class account_voucher(osv.osv):
+
+    _inherit = "account.voucher"
+
+
+    def basic_onchange_partner(self, cr, uid, ids, partner_id, journal_id, ttype, context=None):
+        res = super(account_voucher, self).basic_onchange_partner(cr, uid, ids, partner_id, journal_id, ttype, context=context)
+        journal_pool = self.pool.get('account.journal')
+        res = {'value': {'account_id': False}}
+        if journal_id:
+            journal = journal_pool.browse(cr, uid, journal_id, context=context)
+            if ttype == 'receipt':
+                account_id = journal.default_debit_account_id.id or journal.default_credit_account_id.id
+            elif ttype == 'payment':
+                account_id = journal.default_credit_account_id.id or journal.default_debit_account_id.id
+            res['value']['account_id'] = account_id
+        return res    
