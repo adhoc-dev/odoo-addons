a1b3d3e71ff4a7ac42e8a86d21c7ade73c6573d5
nicomacr <nmr@ingadhoc.com>
2014-10-06 13:07:55 -0300

    Merge

---
+++ account_invoice_company_search/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Account Invioce Company Search",
+    "version": "1.0",
+    'author':  'Ingeniería ADHOC',
+    'website': 'www.ingadhoc.com.ar',
+    "category": "Accounting",
+    "description": """
+Account Invioce Company Search
+============================
+Adds company search filter in account invoice view
+    """,
+    'depends': [
+        'account'
+        ],
+    'data': [
+    'views/invoice_view.xml'
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
