27cc3c65b2183fd43e7fab26be24d578ef01af1f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-06 17:13:05 -0300

    IMP evaluation calaculation

--- evaluation/survey.py
+++ evaluation/survey.py
@@ -25,8 +25,19 @@ class survey_question(osv.Model):
     _inherit = 'survey.question'
 
     @api.one
-    @api.depends('score_ranges_ids','score_ranges_ids.score','score_calc_method','type','copy_labels_ids','copy_labels_ids.score','matrix_subtype','labels_ids_2','labels_ids_2.matrix_answer_score_ids','labels_ids_2.matrix_answer_score_ids.score')
+    @api.depends(
+        'score_ranges_ids',
+        'score_ranges_ids.score',
+        'score_calc_method',
+        'copy_labels_ids',
+        'copy_labels_ids.score',
+        'matrix_subtype',
+        'labels_ids_2',
+        'labels_ids_2.matrix_answer_score_ids',
+        'labels_ids_2.matrix_answer_score_ids.score',
+        'type',)
     def _get_max_score(self):
+        # TODO mejorar esto y ver porque se llama varias veces a esta funcion *hay que cambiarlo tambien en academic_reports que sobreescribimos esta funcion
         max_score = 0
         question = self 
         if question.type == 'simple_choice':
@@ -55,8 +66,9 @@ class survey_question(osv.Model):
         elif question.type == 'matrix' and question.score_calc_method == 'ranges':
             scores = [score_range.score for score_range in question.score_ranges_ids]
             max_score = max(scores if scores else [0])
-        print 'Changing max_score'
+        print 'Changing max_score', self.id, self.survey_id.id
         self.max_score = max_score
+
     
     max_score = new_fields.Integer(compute='_get_max_score', string='Max Score', help='Max score an answer of this question can get', store=True)
 
@@ -115,33 +127,36 @@ class survey_user_input(osv.Model):
     def compute_score(self, cr, uid, ids, context=None):
         if not context:
             context={}
+        question_obj = self.pool['survey.question']
+        user_input_lines_obj = self.pool.get('survey.user_input_line')
+        uiqs_obj = self.pool.get('survey.user_input_question_score')
         for user_input in self.browse(cr, uid, ids, context=context):
             computed_score = 0
-            for question in self.get_questions_of_user_input_survey(user_input):
-                user_input_lines = self.get_user_input_lines_of_question(cr, uid, user_input, question, context=context)
+            question_ids = question_obj.search(
+                cr, uid,
+                [('survey_id', '=', user_input.survey_id.id),
+                 ('max_score', '!=', 0)])
+            for question in question_obj.browse(cr, uid, question_ids, context=context):
+                user_input_lines_ids = user_input_lines_obj.search(cr, uid,
+                    [('question_id', '=', question.id), ('user_input_id', '=', user_input.id)],
+                    context=context)
+                user_input_lines = user_input_lines_obj.browse(cr, uid, user_input_lines_ids, context=context)
+
                 if question.type == 'simple_choice' or question.score_calc_method == 'direct_sum':
                     question_score = sum([self.get_answer_score(user_input_line) for user_input_line in user_input_lines])
                 elif question.score_calc_method == 'ranges':
                     pre_score = sum([self.get_answer_score(user_input_line) for user_input_line in user_input_lines])
-                    # TODO remove this line if score is calculted correctly
-                    # pre_score = sum([user_input_line.value_suggested.score for user_input_line in user_input_lines])
                     question_score = self.get_ranged_score(cr, uid, question, pre_score, context=context)
 
                 computed_score += question_score
 
-                uiqs_obj = self.pool.get('survey.user_input_question_score')
                 uiqs_ids = uiqs_obj.search(cr, uid,
                     [('question_id', '=', question.id), ('user_input_id', '=', user_input.id)],
                     context=context)
-                print 'uiqs_ids', uiqs_ids
 
                 # Calculte score_percentage
-                if question.max_score:
-                    score_percentage = question_score * 100.0 / question.max_score 
-                else:
-                    score_percentage = 0
+                score_percentage = question_score * 100.0 / question.max_score 
 
-                # Create a new input line question record or write de existing one
                 if uiqs_ids:
                     uiqs_obj.write(cr, uid, uiqs_ids[0], {'score': question_score,'score_percentage': score_percentage}, context=context)
                 else:
@@ -152,12 +167,10 @@ class survey_user_input(osv.Model):
                         'user_input_id': user_input.id,
                         }, context=context)    
             if user_input.survey_id.max_score and user_input.survey_id.max_score != 0:
-                print 'user_input.survey_id.max_score', user_input.survey_id.max_score
-                print 'computed_score', computed_score
                 computed_score = computed_score * 100.0 / user_input.survey_id.max_score
-                print 'computed_score', computed_score
             else: 
                 computed_score = False
+            print 'User Input Computed Score', computed_score
             self.write(cr, uid, user_input.id, {'score': computed_score})
         return True
 
@@ -172,19 +185,6 @@ class survey_user_input(osv.Model):
         else:
             return question_score_range_obj.browse(cr, uid, question_score_range_ids[0], context=context).score
 
-    def get_user_input_lines_of_question(self, cr, uid, user_input, question, context=None):
-        user_input_lines_obj = self.pool.get('survey.user_input_line')
-        user_input_lines_ids = user_input_lines_obj.search(cr, uid,
-            [('question_id', '=', question.id), ('user_input_id', '=', user_input.id)],
-            context=context)
-        return user_input_lines_obj.browse(cr, uid, user_input_lines_ids, context=context)
-
-    def get_questions_of_user_input_survey(self, user_input):
-        questions = []
-        for page in user_input.survey_id.page_ids:
-            questions += page.question_ids
-        return questions
-
     def get_answer_score(self, user_input_line):
         if user_input_line.question_id.type in ['simple_choice', 'multiple_choice'] and user_input_line.value_suggested:
             return user_input_line.value_suggested.score
@@ -198,9 +198,11 @@ class survey_user_input(osv.Model):
 
     def write(self, cr, uid, ids, vals, context=None):
         write_res = super(survey_user_input, self).write(cr, uid, ids, vals, context=context)
-        if vals.get('state')=='done':
+        # If score in context then score is being writed
+        if vals.get('state')=='done' and 'score' not in vals:
             if isinstance(ids, (int, long)):
                 ids = [ids]            
+            print 'cuidado!!!!!'
             self.compute_score(cr, uid, ids, context=context)
         return write_res
 
@@ -438,8 +440,9 @@ class survey_survey(osv.Model):
     }
     # Metodo por si queremos recalcular todos los score
     def compute_score(self, cr, uid, ids, context=None):    
-        for survey in self.browse(cr, uid, ids, context=context):
-            user_input_ids = [x.id for x in survey.user_input_ids]
-            print 'compute_score user input ids: ', user_input_ids
-            self.pool.get('survey.user_input').compute_score(cr, uid, user_input_ids, context=context)
-        return True
+        user_input_obj = self.pool['survey.user_input']
+        total_user_input_ids = user_input_obj.search(
+            cr, uid, [('survey_id', 'in', ids)], order='id', context=context)
+        print 'len total_user_input_ids', len(total_user_input_ids)
+        user_input_obj.compute_score(cr, uid, total_user_input_ids, context=context)
+        return total_user_input_ids
