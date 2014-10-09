b39bd9bacd16ece021ecc243afa3e7ef0fd513d0
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-09 11:37:30 -0300

    IMP product salesman group

--- product_salesman_group/product.py
+++ product_salesman_group/product.py
@@ -2,14 +2,14 @@
 from openerp.osv import fields, osv
 
 
-class product_product(osv.osv):
+class product_template(osv.osv):
 
-    _inherit = 'product.product'
+    _inherit = 'product.template'
 
     _columns = {
         'salesman_group_ids': fields.many2many(
-            'sale.salesman.group', 'productsalesgroup_rel',
-            'product_id', 'section_id', string='Salesman Group', ),
+            'sale.salesman.group', 'prod_template_salesgroup_rel',
+            'template_id', 'section_id', string='Salesman Group', ),
     }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
