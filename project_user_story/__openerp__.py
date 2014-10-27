98d1238067e28cb0705739140bbbd3b60795c210
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-27 16:34:19 -0300

    ADD project user story

---
+++ project_user_story/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Project User Stories',
+    'version': '1.0',
+    'category': 'Projects & Services',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Project User Stories
+====================
+    """,
+    'author':  'ADHOC SA',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'project_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ project_user_story/project.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+
+
+class task(models.Model):
+    _inherit = 'project.task'
+
+    user_story = fields.Boolean(
+        'Is User Story?',
+        default=False)
+
+    @api.multi
+    def action_open_task(self):
+        return {
+            'name': _('User Story'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            # 'view_id': [res_id],
+            'res_model': 'project.task',
+            # 'context': "{'type':'out_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': self.id,
+        }
+
+
+class project(models.Model):
+    _inherit = 'project.project'
+
+    user_story_ids = fields.One2many(
+        'project.task',
+        'project_id',
+        domain=[('user_story', '=', True)],
+        context={'default_user_story': True},
+        string='User Stories',
+    )
