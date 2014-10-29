ae5fb8e7440cd9fbaa669489941c6e3cf9e44c85
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-29 18:58:07 -0300

    ADD product website_catalog_search

---
+++ product_website_categ_search/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{'active': False,
+    'author': 'Ingenieria ADHOC.',
+    'category': 'base.module_category_knowledge_management',
+    'demo_xml': [],
+    'depends': ['product'],
+    'description': """
+Product Search by Website Category
+==================================
+""",
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'Product Search by Website Category',
+    'test': [],
+    'data': [
+        'product_view.xml',
+    ],
+    'website': 'www.ingadhoc.com'}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
