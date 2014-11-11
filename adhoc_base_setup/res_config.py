dd75160f0ede23d429b5e07b0ec379a53fc59392
nicomacr <nmr@ingadhoc.com>
2014-11-11 13:37:30 -0300

    IMP Fix related to invoice refund generation

--- adhoc_base_setup/res_config.py
+++ adhoc_base_setup/res_config.py
@@ -64,6 +64,8 @@ class adhoc_base_configuration(osv.osv_memory):
             help="""Installs the account_bank_voucher module."""),
         'module_account_invoice_prices_update': fields.boolean('Allow prices update on invoices based on pricelist',
             help="""Installs the account_invoice_prices_update module."""),
+        'module_account_refund_invoice_fix': fields.boolean('Fix related to invoice refund generation',
+            help="""Installs the account_refund_invoice_fix module."""),
         
         # Sale / Purchase modules
         'module_purchase_discounts': fields.boolean('Mange disccounts on purchases',
