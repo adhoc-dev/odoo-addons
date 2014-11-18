8d0d50426b3edc1141fa3b0364f86358f3a3b1fe
nicomacr <nmr@ingadhoc.com>
2014-11-18 11:30:00 -0300

    FIX account_invoice_refund_fix

--- account_refund_invoice_fix/account_invoice_refund.py
+++ account_refund_invoice_fix/account_invoice_refund.py
@@ -1,11 +1,51 @@
 # -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
 
-from openerp import models
-
-
-class account_invoice_refund(models.Model):
+class account_invoice_refund(models.TransientModel):
 
     _inherit = 'account.invoice.refund'
+
+   
+    @api.model
+    def _get_invoice_id(self):
+        return self._context.get('active_id', False)
+
+    @api.one
+    @api.depends('invoice_id')
+    def _get_journal_type(self):
+        if self.invoice_id.type == 'out_refund' or 'out_invoice':
+            self.journal_type = 'sale_refund'
+        elif self.invoice_id.type == 'in_refund' or 'in_invoice':
+            self.journal_type = 'purchase_refund'
+
+    invoice_id = fields.Many2one(
+        'account.invoice',
+        'Invoice',
+        default=_get_invoice_id,
+        store=True)
+
+   
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        related='invoice_id.company_id'
+        )
+
+    journal_type = fields.Char(
+        'Journal type',
+        compute='_get_journal_type'
+        )
+
+    
+    @api.model
+    def _get_journal(self):
+        company_id = self.company_id
+        journal = self.env['account.journal'].search([('type', '=', self.journal_type), ('company_id','=',self.company_id.id)], limit=1)
+        return journal and journal[0] or False
+
+    _defaults = {
+        'journal_id': _get_journal,
+    }
 
     def compute_refund(self, cr, uid, ids, data_refund, context=None):
         res = super(account_invoice_refund, self).compute_refund(
@@ -20,7 +60,13 @@ class account_invoice_refund(models.Model):
         refund_invoice_ids = invoice_obj.search(cr, uid, domain)
         origin = ', '.join([x.number for x in invoice_obj.browse(
             cr, uid, invoice_ids) if x.number])
-        invoice_obj.write(cr, uid, refund_invoice_ids, {'origin': origin})
+        invoice_obj.write(cr, uid, refund_invoice_ids, {
+            'origin': origin,
+            })
+        if not self.browse(cr, uid ,ids, context=context)[0].period:
+            invoice_obj.write(cr, uid, refund_invoice_ids, {
+            'period_id': invoice_obj.browse(cr, uid, invoice_ids)[0].period_id.id
+            })
         for invoice_id in refund_invoice_ids:
             self.pool['sale.order'].write(
                 cr, uid, sale_order_ids, {'invoice_ids': [(4, invoice_id)]})
