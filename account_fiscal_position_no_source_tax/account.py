828be5ee4640ddd9ee595b4ba15fa973ccbcb82f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-11-18 17:08:17 -0300

    FIX fiscal position no source tax on v7 api

--- account_fiscal_position_no_source_tax/account.py
+++ account_fiscal_position_no_source_tax/account.py
@@ -5,6 +5,15 @@ class account_fiscal_position(models.Model):
 
     _inherit = 'account.fiscal.position'
 
+    @api.v7
+    def map_tax(self, cr, uid, fposition_id, taxes, context=None):
+        result = super(account_fiscal_position, self).map_tax(
+            cr, uid, fposition_id, taxes, context=context)
+        taxes_without_src_ids = [
+            x.tax_dest_id.id for x in fposition_id.tax_ids if not x.tax_src_id]
+        result = set(result) | set(taxes_without_src_ids)
+        return list(result)
+
     @api.v8     # noqa
     def map_tax(self, taxes):
         result = super(account_fiscal_position, self).map_tax(taxes)
