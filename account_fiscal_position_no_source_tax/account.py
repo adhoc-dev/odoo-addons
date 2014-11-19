de56d3d78f546750849d2ba915e426a5e40c5d8d
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-11-19 10:13:33 -0300

    FIX fiscal position no source tax

--- account_fiscal_position_no_source_tax/account.py
+++ account_fiscal_position_no_source_tax/account.py
@@ -9,9 +9,11 @@ class account_fiscal_position(models.Model):
     def map_tax(self, cr, uid, fposition_id, taxes, context=None):
         result = super(account_fiscal_position, self).map_tax(
             cr, uid, fposition_id, taxes, context=context)
-        taxes_without_src_ids = [
-            x.tax_dest_id.id for x in fposition_id.tax_ids if not x.tax_src_id]
-        result = set(result) | set(taxes_without_src_ids)
+        print 'fposition_id', fposition_id
+        if fposition_id:
+            taxes_without_src_ids = [
+                x.tax_dest_id.id for x in fposition_id.tax_ids if not x.tax_src_id]
+            result = set(result) | set(taxes_without_src_ids)
         return list(result)
 
     @api.v8     # noqa
