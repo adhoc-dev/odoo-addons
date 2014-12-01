1757d906023f046833e9e0a4e2c8b3a6e2c56076
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-12-01 12:16:24 -0300

    ADD crm phonceall type

---
+++ crm_phonecall_type/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{'author': 'Ingenieria ADHOC',
+    'demo_xml': [
+    ],
+    'depends': [
+        'crm'
+        ],
+    'description': 'CRM Phonecall Type',
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'CRM Phonecall Type',
+    'test': [],
+    'data': [
+        'crm_phonecall_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'version': 'No version',
+    'auto_install': False,
+    'category': '',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
