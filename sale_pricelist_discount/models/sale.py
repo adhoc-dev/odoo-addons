04725d5fd83561fa564d3f0a5ab19d732cbb1864
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-12-13 11:17:54 -0300

    IMP sale pricelist_discount

--- sale_pricelist_discount/models/sale.py
+++ sale_pricelist_discount/models/sale.py
@@ -11,10 +11,18 @@ class sale_order_line(models.Model):
         if self.product_id.list_price:
             list_price = self.with_context(
                 currency_id=self.order_id.pricelist_id.currency_id.id
-                ).product_id.price_get()[self.product_id.id]
+            ).product_id.price_get()[self.product_id.id]
             discount = (list_price - self.price_unit) * 100.0 / list_price
+
+        total_discount = discount + self.discount - (
+            discount * self.discount or 0.0)
         self.pricelist_discount = discount
+        self.total_discount = total_discount
 
     pricelist_discount = fields.Float(
         compute='_get_pricelist_discount',
         string='Pricelist Discount')
+
+    total_discount = fields.Float(
+        compute='_get_pricelist_discount',
+        string='Total Discount')
