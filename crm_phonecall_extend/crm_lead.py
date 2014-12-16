56b972dd82afcf63f114e5414167cb8bcfe57c6c
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-12-16 11:19:09 -0300

    IMP partner area

--- crm_phonecall_extend/crm_lead.py
+++ crm_phonecall_extend/crm_lead.py
@@ -2,36 +2,50 @@
 from openerp.osv import fields, osv
 
 
-
-
 class crm_lead(osv.osv):
     _inherit = "crm.lead"
 
     def _get_phonecall(self, cr, uid, ids, name, args, context=None):
         result = {}
-        phonecall_obj=self.pool['crm.phonecall']
+        phonecall_obj = self.pool['crm.phonecall']
         for lead in self.browse(cr, uid, ids, context=context):
             phonecall_id = False
             categ_id = False
             user_id = False
-            phonecall_ids = phonecall_obj.search(cr, uid, [('opportunity_id', '=', lead.id ),('state', '=', 'pending')], order='date desc', context=context)
+            phonecall_ids = phonecall_obj.search(cr, uid, [(
+                'opportunity_id', '=', lead.id),
+                ('state', 'in', ('open', 'pending'))],
+                order='date desc', context=context)
             if phonecall_ids:
                 phonecall_id = phonecall_ids[0]
-                categ_id= phonecall_obj.browse(cr, uid, phonecall_id, context=context).categ_id.id
-                user_id = phonecall_obj.browse(cr, uid, phonecall_id, context=context).user_id.id
+                categ_id = phonecall_obj.browse(
+                    cr, uid, phonecall_id, context=context).categ_id.id
+                user_id = phonecall_obj.browse(
+                    cr, uid, phonecall_id, context=context).user_id.id
             result[lead.id] = {
-                    'next_phonecall_id': phonecall_id,
-                    'phonecall_categ_id': categ_id,
-                    'phonecall_user_id': user_id,
-                }
+                'next_phonecall_id': phonecall_id,
+                'phonecall_categ_id': categ_id,
+                'phonecall_user_id': user_id,
+            }
         return result
-    
+
     _columns = {
-
-        'next_phonecall_id': fields.function(_get_phonecall,
-            type='many2one',string='Next Phonecall', multi="call", relation='crm.phonecall'
-            ),
-        'phonecall_categ_id':fields.function(_get_phonecall, type='many2one', string='Category Phonecall',multi="call",relation='crm.case.categ'),
-        'phonecall_user_id':fields.function(_get_phonecall, type='many2one', string='Users Phonecall', multi="call",relation='res.users'),
-
+        'next_phonecall_id': fields.function(
+            _get_phonecall,
+            type='many2one',
+            string='Next Phonecall',
+            multi="call",
+            relation='crm.phonecall'),
+        'phonecall_categ_id': fields.function(
+            _get_phonecall,
+            type='many2one',
+            string='Category Phonecall',
+            multi="call",
+            relation='crm.case.categ'),
+        'phonecall_user_id': fields.function(
+            _get_phonecall,
+            type='many2one',
+            string='Users Phonecall',
+            multi="call",
+            relation='res.users'),
     }
--- partner_area/partner_area.py
+++ partner_area/partner_area.py
@@ -1,27 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Ingenieria ADHOC - ADHOC SA
-#    https://launchpad.net/~ingenieria-adhoc
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-
-import openerp
-from openerp import SUPERUSER_ID
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
@@ -29,28 +6,27 @@ from openerp.tools.translate import _
 class partner_area(osv.osv):
     _name = 'res.partner.area'
     _description = 'Area'
-    
-        
     _columns = {
-        'name': fields.char( string='Name',required=True),
-        }
+        'name': fields.char(string='Name', required=True),
+    }
 
-
-
-
+    def create(self, cr, uid, vals, context=None):
+        if 'from_m2m' not in context:
+            raise osv.except_osv(
+                _('Warning!'), _('You can only create Areas from companies!'))
+        return super(partner_area, self).create(cr, uid, vals, context=context)
 
 
 class res_partner(osv.osv):
     _inherit = "res.partner"
 
-    
-        
     _columns = {
-        'area_ids': fields.many2many('res.partner.area', string='Areas'),
-        'parent_area_ids': fields.related('area_ids', 'parent_id', string='Parent Areas',type="one2many", relation="res.partner.area"),
+        'area_ids': fields.many2many(
+            'res.partner.area', string='Areas'),
+        'parent_area_ids': fields.related(
+            'parent_id', 'area_ids', string='Parent Areas',
+            type="many2many", relation="res.partner.area"),
         'area_id': fields.many2one('res.partner.area', string='Area'),
     }
 
-   
-        
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
