b56517dd4672fcf2dad691d57f0b8ddbf29233e7
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-12-24 12:15:03 -0300

    IMP remove unused code on auth admin passkey

--- auth_server_admin_passwd_passkey/__openerp__.py
+++ auth_server_admin_passwd_passkey/__openerp__.py
@@ -1,25 +1,4 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Admin Passkey module for OpenERP
-#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 {
     'name': 'Authentification - Admin Passkey',
     'version': '2.1.1',
@@ -31,27 +10,14 @@ Server Admin password become a passkey for all active logins
 Functionality :
 ---------------
     * You can now login with any user using server admin password (admin_passwd parameter)
-    * TODO: You can choose odoo to send a mail to user and/or admin to indicate them;
-
-Technical information :
------------------------
-    * TODO: Create two ir_config_parameter to enable / disable mail sending;
-
-Copyright, Author and Licence :
--------------------------------
-    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
-    * Author : Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
+    'author': 'ADHOC SA',
+    'website': 'www.ingadhoc.com',
     'license': 'AGPL-3',
     'depends': [
-        'mail',
+        'base',
         ],
     'data': [
-        # 'data/ir_config_parameter.xml',
-        # 'view/res_config_view.xml',
     ],
     'demo': [],
     'js': [],
