24b93c80e4e44885dc1f8c7ad6e510f615da5903
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-01-26 11:36:05 -0300

    FIX pricelist discount in sale.order and invoice

---
+++ account_invoice_pricelist_discount/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Invoice Pricelist Discount',
+    'version': '2.0',
+    'description': """
+Account Invoice Pricelist Discount
+==================================
+""",
+    'category': 'Account',
+    'author': 'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/invoice_view.xml',
+        ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sale_pricelist_discount/models/sale.py
+++ sale_pricelist_discount/models/sale.py
@@ -1,28 +1,44 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models, api
+import openerp.addons.decimal_precision as dp
 
 
 class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
 
+    # @api.one
+    # def _get_pricelist_discount(self):
+    #     discount = 0.0
+    #     if self.product_id.list_price:
+    #         list_price = self.with_context(
+    #             currency_id=self.order_id.pricelist_id.currency_id.id
+    #         ).product_id.price_get()[self.product_id.id]
+    #         discount = (list_price - self.price_unit) * 100.0 / list_price
+
+    #     total_discount = discount + self.discount - (
+    #         discount * self.discount or 0.0) / 100.0
+    #     self.pricelist_discount = discount
+    #     self.total_discount = total_discount
+
     @api.one
-    def _get_pricelist_discount(self):
-        discount = 0.0
+    def _get_list_price(self):
+        list_price = 0.0
         if self.product_id.list_price:
             list_price = self.with_context(
                 currency_id=self.order_id.pricelist_id.currency_id.id
-            ).product_id.price_get()[self.product_id.id]
-            discount = (list_price - self.price_unit) * 100.0 / list_price
+            ).product_id.price_get()
+        print list_price
+        self.list_price = list_price[self.product_id.id]
 
-        total_discount = discount + self.discount - (
-            discount * self.discount or 0.0) / 100.0
-        self.pricelist_discount = discount
-        self.total_discount = total_discount
+    list_price = fields.Float(
+        compute='_get_list_price',
+        digits_compute=dp.get_precision('Product Price'),
+        string='List Price')
 
-    pricelist_discount = fields.Float(
-        compute='_get_pricelist_discount',
-        string='Pricelist Discount')
+    # pricelist_discount = fields.Float(
+    #     compute='_get_pricelist_discount',
+    #     string='Pricelist Discount')
 
-    total_discount = fields.Float(
-        compute='_get_pricelist_discount',
-        string='Total Discount')
+    # total_discount = fields.Float(
+    #     compute='_get_pricelist_discount',
+    #     string='Total Discount')
