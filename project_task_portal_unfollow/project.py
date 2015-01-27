f2a76890bc34c108e5b42a94faea00c6cea631a5
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-01-27 11:03:09 -0300

    FIX proyect_task_portal_unfollow

--- project_task_portal_unfollow/project.py
+++ project_task_portal_unfollow/project.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import fields, osv
+from openerp import models
 
 
-class task(osv.osv):
+class task(models.Model):
     _inherit = 'project.task'
 
     def create(self, cr, uid, vals, context=None):
-             
+
         task_id = super(task, self).create(cr, uid, vals, context=context)
-        
+
         partner_to_unfollow_ids = []
-        for partner in self.browse(cr, uid, task_id, context=context).message_follower_ids:
+        for partner in self.browse(
+                cr, uid, task_id, context=context).message_follower_ids:
             if partner.user_ids:
                 for user in partner.user_ids:
-                    if not self.pool['res.users'].has_group(cr, user.id, 'base.group_user'):
+                    if not self.pool['res.users'].has_group(
+                            cr, user.id, 'base.group_user'):
                         partner_to_unfollow_ids.append(partner.id)
                         continue
             else:
                 partner_to_unfollow_ids.append(partner.id)
-        self.message_unsubscribe(cr, uid, [task_id], partner_to_unfollow_ids, context=None)
+        self.message_unsubscribe(
+            cr, uid, [task_id], partner_to_unfollow_ids, context=None)
         return task_id
