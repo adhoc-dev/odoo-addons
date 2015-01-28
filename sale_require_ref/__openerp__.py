80e8845e7a3baca171ddeefd0bc9ebf5771b04ba
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-27 21:08:02 -0300

    ADD sale requrie ref

---
+++ sale_require_ref/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Require Contract on Confirmation',
+    'version': '1.0',
+    'category': 'Projects & Services',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Sale Order Require Contract on Confirmation
+===========================================
+    """,
+    'author':  'ADHOC SA',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
