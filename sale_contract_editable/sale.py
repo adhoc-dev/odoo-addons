7436268c044ed2790768f1c345c584557a6655d8
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-29 16:29:15 -0300

    FIX contract editable

--- sale_contract_editable/sale.py
+++ sale_contract_editable/sale.py
@@ -5,4 +5,4 @@ from openerp import models, fields
 class sale_order(models.Model):
     _inherit = "sale.order"
 
-    project_id = fields.Many2one(states={})
+    project_id = fields.Many2one(states={}, readonly=False)
