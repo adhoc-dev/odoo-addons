e58de53814af255f05cfe2bda60c54e2669ef087
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-02-23 21:02:41 -0300

    IMP discounts visible

--- sale_pricelist_discount/models/sale.py
+++ sale_pricelist_discount/models/sale.py
@@ -11,9 +11,23 @@ class sale_order_line(models.Model):
         price_get = self.with_context(
             currency_id=self.order_id.pricelist_id.currency_id.id
         ).product_id.price_get()
-        self.list_price = price_get and price_get[self.product_id.id] or 0.0
+        list_price = price_get and price_get[self.product_id.id] or 0.0
+        discount = list_price and (
+            (list_price - self.price_unit) * 100.0 / list_price) or 0.0
+        total_discount = discount + self.discount - (
+            discount * self.discount or 0.0) / 100.0
+
+        self.list_price = list_price
+        self.list_discount = discount
+        self.total_discount = total_discount
 
     list_price = fields.Float(
         compute='_get_list_price',
         digits_compute=dp.get_precision('Product Price'),
         string='List Price')
+    list_discount = fields.Float(
+        compute='_get_list_price',
+        string='List Discount')
+    total_discount = fields.Float(
+        compute='_get_list_price',
+        string='Total Discount')
--- sale_prices_update/sale_order.py
+++ sale_prices_update/sale_order.py
@@ -1,22 +1,20 @@
 # -*- encoding: utf-8 -*-
+from openerp import models
 
-from openerp.osv import osv
-from openerp.osv import fields
 
-class sale_order(osv.osv):
+class sale_order(models.Model):
     _inherit = 'sale.order'
-       
+
     def update_prices(self, cr, uid, ids, context=None):
         order_line_obj = self.pool.get('sale.order.line')
         for sale in self.browse(cr, uid, ids):
-            items = []
             for line in sale.order_line:
-                price = self.pool.get('product.pricelist').price_get(cr, uid, [sale.pricelist_id.id],
-                    line.product_id.id, line.product_uom_qty or 1.0, sale.partner_id.id, {
+                price = self.pool.get('product.pricelist').price_get(
+                    cr, uid, [sale.pricelist_id.id],
+                    line.product_id.id, line.product_uom_qty or 1.0,
+                    sale.partner_id.id, {
                         'uom': line.product_uom.id,
                         'date': sale.date_order,
-                        })[sale.pricelist_id.id]
+                    })[sale.pricelist_id.id]
                 order_line_obj.write(cr, uid, [line.id], {'price_unit': price})
         return True
-    
-sale_order()
