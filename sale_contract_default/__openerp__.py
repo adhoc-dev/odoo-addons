3f56cc6582fc19549aae43035ebaa928fe3bdef0
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-03-06 12:41:39 -0300

    FIX concatenating the name of the contract with the name of the Partner

---
+++ sale_contract_default/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sale Contract Default",
+    'version': '1.0',
+    'category': 'Accounting',
+    'sequence': 14,
+    'summary': 'Quotations, Sales Orders, Invoicing',
+    'description': """
+Sale Contract Default
+====================
+
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
