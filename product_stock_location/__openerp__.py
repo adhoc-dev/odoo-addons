a64868f94223842184213761f8525155e401a1d8
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-03-10 12:32:50 -0300

    FIX Filter quantities by location in view Product List

---
+++ product_stock_location/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Stock Location',
+    'version': '1.0',
+    'category': 'Sales Management',
+    'sequence': 14,
+    'summary': 'Sales, Product, Category, Clasification',
+    'description': """
+Product Stock Location
+======================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'product_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
