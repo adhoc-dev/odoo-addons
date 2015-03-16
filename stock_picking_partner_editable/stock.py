8867dd808432e57c3a6e3791f0d6de3fe13bca5a
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-03-16 17:32:26 -0300

    FIX stock picking partner editable

--- stock_picking_partner_editable/stock.py
+++ stock_picking_partner_editable/stock.py
@@ -3,7 +3,7 @@ from openerp import models, fields
 
 
 class stock_picking(models.Model):
-    _inherit = 'stock.inventory'
+    _inherit = 'stock.picking'
 
     partner_id = fields.Many2one(
         states={'cancel': [('readonly', True)]})
