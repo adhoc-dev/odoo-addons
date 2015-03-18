1bcf78ab4a7cae5ecb7fcc7ecdbc6fe905f54c38
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-03-18 16:57:18 -0300

    FIX account partner balance

--- account_partner_balance/partner.py
+++ account_partner_balance/partner.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
+from openerp import fields, models, api
 
 
-from operator import itemgetter
-import time
-
-from openerp.osv import fields, osv
-
-class res_partner(osv.osv):
+class res_partner(models.Model):
     _inherit = 'res.partner'
 
-    def _balance(self, cr, uid, ids, field_name, args,context=None):
-        res = {}
-        for line in self.browse(cr, uid, ids, context=None):
-            res[line.id] = line.debit + line.credit
-        return res
+    balance = fields.Float(
+        compute='_get_balance',
+        string='Balance',
+        )
 
-    _columns = {
-        # 'net': fields.function(_net, store={'account.move.line': (lambda self, cr, uid, ids, c={}: ids,
-                # ['debit', 'credit'],10)}, method=True, string='Net'),     
-        # quise hacer la funcion con store pero el problema es que solo me calcula cuando modifico un campo y no me hace el calculo la primer vez
-        'balance': fields.function(_balance, string='Balance'),        
-    }
+    @api.one
+    @api.depends('debit', 'credit')
+    def _get_balance(self):
+        self.balance = self.debit - self.credit
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
