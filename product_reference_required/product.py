4e27e9f41c1c9c96113d08dc049431dee1399600
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-03-19 12:50:31 -0300

    IMP make reference requiere to product also

--- product_reference_required/product.py
+++ product_reference_required/product.py
@@ -2,6 +2,14 @@
 from openerp import models, fields
 
 
+class product_template(models.Model):
+    _inherit = "product.template"
+
+    default_code = fields.Char(
+        required=True,
+        )
+
+
 class product_product(models.Model):
     _inherit = "product.product"
 
