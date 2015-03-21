b8821d3d13728588a7bfecd46104ae7ea4d6957f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-03-20 21:32:12 -0300

    ADD intercompany documento to allow intercompany move uninstall

--- inter_company_move/models/__init__.py
+++ inter_company_move/models/__init__.py
@@ -2,3 +2,4 @@
 import res_company
 import inter_company_invoice
 import invoice_report
+import intercompany_document
---
+++ inter_company_move/models/intercompany_document.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openerp import models
+
+
+class intercompany_document(models.Model):
+    _name = 'intercompany.document'
