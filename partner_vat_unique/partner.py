c01f000bbc50420e4f817dd7fc964f7e2de078be
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-03-25 18:15:28 -0300

    IMP partner_vat_unique/partner.py

--- partner_vat_unique/partner.py
+++ partner_vat_unique/partner.py
@@ -7,7 +7,7 @@ class res_partner(models.Model):
     _inherit = "res.partner"
 
     @api.one
-    @api.constrains('vat', 'parent_id')
+    @api.constrains('vat', 'parent_id', 'company_id')
     def check_vat_unique(self):
         if not self.vat:
             return True
@@ -20,18 +20,21 @@ class res_partner(models.Model):
         same_vat_partners = self.search([
             ('vat', '=', self.vat),
             ('vat', '!=', False),
+            ('company_id', '=', self.company_id.id),
             ])
 
         if same_vat_partners:
             related_partners = self.search([
                 ('id', 'child_of', parent.id),
+                ('company_id', '=', self.company_id.id),
                 ])
             same_vat_partners = self.search([
                 ('id', 'in', same_vat_partners.ids),
                 ('id', 'not in', related_partners.ids),
+                ('company_id', '=', self.company_id.id),
                 ])
             if same_vat_partners:
                 raise Warning(_(
-                    'Partner vat must be unique except on partner with parent/childe relationship. Partners with same vat and not related, are: %s!') % (', '.join(x.name for x in same_vat_partners)))
+                    'Partner vat must be unique per company except on partner with parent/childe relationship. Partners with same vat and not related, are: %s!') % (', '.join(x.name for x in same_vat_partners)))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
