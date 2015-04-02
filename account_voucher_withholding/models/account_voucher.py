75d8fadd317790b6785fa06ae0e2520124f83ef8
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-04-02 16:24:33 -0300

    IMP withholding

--- account_voucher_withholding/models/account_voucher.py
+++ account_voucher_withholding/models/account_voucher.py
@@ -28,12 +28,32 @@ class account_voucher(models.Model):
         states={'draft': [('readonly', False)]}
         )
 
-    @api.one
-    def add_withholding(self):
-        """For each withholding_ids (withholding) it creates an
-        account.move.line with:
-            Partner: withholding voucher partner
-            Account: withholding tax_withholding_id account
-            Debit/credit: withholding amount
-        """
-        return None
+    @api.model
+    def first_move_line_get(
+            self, voucher_id, move_id, company_currency, current_currency):
+        res = super(account_voucher, self).first_move_line_get(
+            voucher_id, move_id, company_currency, current_currency)
+        print 'bbbbbbbbbbbbbbbbbbbb'
+        print 'bbbbbbbbbbbbbbbbbbbb'
+        return res
+
+    @api.model
+    def voucher_move_line_create(
+            self, voucher_id, line_total, move_id, company_currency,
+            current_currency):
+        print 'aaaaaaaaaaaaaaaaaaaa'
+        print 'aaaaaaaaaaaaaaaaaaaa'
+        res = super(account_voucher, self).voucher_move_line_create(
+            voucher_id, line_total, move_id, company_currency,
+            current_currency)
+        return res
+
+    # @api.one
+    # def add_withholding(self):
+    #     """For each withholding_ids (withholding) it creates an
+    #     account.move.line with:
+    #         Partner: withholding voucher partner
+    #         Account: withholding tax_withholding_id account
+    #         Debit/credit: withholding amount
+    #     """
+    #     return None
