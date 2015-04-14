ec468c01662bb47429c8dc9735f7cd5acb59521f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-04-14 18:39:33 -0300

    ADD invoice direct payment

---
+++ account_invoice_direct_payment/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{'active': False,
+    'author':  'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'category': 'Accounting & Finance',
+    'data': [
+        'views/account_journal_view.xml',
+        'views/account_invoice_view.xml',
+    ],
+    'demo': [],
+    'depends': [
+        'account_voucher'
+    ],
+    'description': '''
+Account Invoice Direct Payment
+==============================
+''',
+    'installable': True,
+    'name': 'Account Invoice Direct Payment',
+    'test': [],
+    'version': '1.243',
+ }
