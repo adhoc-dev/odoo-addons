66ef8a9931eef2c14d91d52201b9df0ab5cad111
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-04-21 09:52:37 -0300

    IMP survey conditional questions

---
+++ survey_conditional_questions/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Survey Conditional Questions',
+    'version': '1.0',
+    'category': 'Warehouse Management',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Survey Conditional Questions
+============================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'survey', 'website'
+    ],
+    'data': [
+        'conditional_survey_view.xml',
+        'conditional_survey_qweb.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
