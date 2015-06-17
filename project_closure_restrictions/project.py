db461d26931acce709b782a99540e1d9bb3afa4a
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-06-17 15:13:48 -0300

    IMP project closure

--- project_closure_restrictions/project.py
+++ project_closure_restrictions/project.py
@@ -3,19 +3,20 @@ from openerp import models, api, _
 from openerp.exceptions import Warning
 
 
-class project(models.Model):
+class project_project(models.Model):
     _inherit = 'project.project'
 
-    @api.one
+    @api.multi
     def set_done(self):
-        if not self.analytic_account_id.state == 'close':
-            raise Warning(
-                _("You can not cancel the project if the analytic account is open"))
-        if self.task_ids:
-            tasks_open = self.env['project.task'].search(
-                [('id', 'in', [x.id for x in self.task_ids]), ('stage_id.fold', '!=', True)])
-            if tasks_open:
+        for project in self:
+            if not project.analytic_account_id.state == 'close':
                 raise Warning(
-                    _("You can not close a project with active task, we consider active task the one in stages without option 'folded'"))
+                    _("You can not cancel the project if the analytic account is open"))
+            if project.task_ids:
+                tasks_open = project.env['project.task'].search(
+                    [('id', 'in', [x.id for x in project.task_ids]), ('stage_id.fold', '!=', True)])
+                if tasks_open:
+                    raise Warning(
+                        _("You can not close a project with active task, we consider active task the one in stages without option 'folded'"))
 
-        return super(project, self).set_done()
+        return super(project_project, self).set_done()
