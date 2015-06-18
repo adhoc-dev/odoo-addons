4e49782d3992c4984d2f21758510b669d1730b5c
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-06-18 13:11:26 -0300

    IMP stock ean 128

--- stock_ean128/stock.py
+++ stock_ean128/stock.py
@@ -2,7 +2,7 @@
 from openerp import models, fields, api
 
 
-class stock_picking(models.Model):
+class stock_production_lot(models.Model):
     _inherit = 'stock.production.lot'
 
     ean_128 = fields.Char(string="EAN128")
@@ -19,3 +19,12 @@ class stock_picking(models.Model):
         else:
             name += '(17)' + 'N/A'
         self.ean_128 = name
+
+    def name_search(
+            self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        res = super(stock_production_lot, self).name_search(
+            cr, uid, name=name, args=args, operator=operator, context=context, limit=limit)
+        ids = self.search(cr, uid, [('ean_128', operator, name)],
+                          limit=limit, context=context)
+        res += self.name_get(cr, uid, ids, context=context)
+        return res
