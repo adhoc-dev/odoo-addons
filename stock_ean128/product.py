8cb611f5076c6436f981b65873b73f1ca7451c09
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-07-20 09:16:29 -0300

    fix search lot in product

--- stock_ean128/product.py
+++ stock_ean128/product.py
@@ -19,6 +19,8 @@ class product_template(models.Model):
 
     @api.model
     def search_lots(self, operator, operand):
+        if operand[0].encode('utf8') == ' ':
+            operand = operand[1:]
         templates = self.env['stock.production.lot'].search(
             [('ean_128', operator, operand)]).mapped(
                 'product_id').mapped('product_tmpl_id')
@@ -35,6 +37,8 @@ class product_product(models.Model):
         res = super(product_product, self).name_search(
             name, args=args, operator=operator, limit=limit)
         if len(res) < limit:
+            if name[0].encode('utf8') == ' ':
+                name = name[1:]
             products = self.env['stock.production.lot'].search(
                 [('ean_128', operator, name)],
                 limit=limit).mapped('product_id')
