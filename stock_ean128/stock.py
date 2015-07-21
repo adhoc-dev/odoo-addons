4c6df8b27fe4456c51142506c39bf7ead119d102
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-07-21 09:48:59 -0300

    FIX name search

--- stock_ean128/stock.py
+++ stock_ean128/stock.py
@@ -26,7 +26,9 @@ class stock_production_lot(models.Model):
             self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
         res = super(stock_production_lot, self).name_search(
             cr, uid, name=name, args=args, operator=operator, context=context, limit=limit)
-        ids = self.search(cr, uid, [('ean_128', operator, name)],
-                          limit=limit, context=context)
+        ids = []
+        if name:
+            ids = self.search(cr, uid, [('ean_128', operator, name)],
+                              limit=limit, context=context)
         res += self.name_get(cr, uid, ids, context=context)
         return res
