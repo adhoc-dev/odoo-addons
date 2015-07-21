253075c64541420c8f779ae917d85cfc956ff788
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-21 17:35:44 -0300

    IMP on settlement

--- account_tax_settlement_voucher/models/tax_settlement.py
+++ account_tax_settlement_voucher/models/tax_settlement.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import api, models, _
+from openerp import api, models
 
 
 class account_tax_settlement(models.Model):
@@ -8,27 +8,5 @@ class account_tax_settlement(models.Model):
     @api.multi
     def settlement_pay(self):
         self.ensure_one()
-        view_id = self.env['ir.model.data'].xmlid_to_res_id(
-            'account_voucher.view_vendor_receipt_dialog_form')
-        return {
-            'name': _("Pay Settlement"),
-            'view_mode': 'form',
-            'view_id': view_id,
-            'view_type': 'form',
-            'res_model': 'account.voucher',
-            'type': 'ir.actions.act_window',
-            'nodestroy': True,
-            'target': 'new',
-            'domain': '[]',
-            'context': {
-                'payment_expected_currency': self.company_id.currency_id.id,
-                'default_partner_id': self.partner_id.id,
-                'default_amount': self.residual,
-                'default_reference': self.name,
-                'close_after_process': True,
-                # 'invoice_type': self.type,
-                # 'invoice_id': self.id,
-                'default_type': 'payment',
-                'type': 'payment'
-            }
-        }
+        return self.move_id.with_context(
+            from_settlement=True).create_voucher('payment')
