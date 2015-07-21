8740092c5c6d1fababdd44a7e9e4dbc180403e09
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-21 13:12:47 -0300

    FIX disable mrp auto prod demo data

--- mrp_auto_prod/__openerp__.py
+++ mrp_auto_prod/__openerp__.py
@@ -6,16 +6,16 @@
     'category': 'Localization/Argentina',
     'website':  'www.adhoc.com.ar',
     'license':  'AGPL-3',
-    'description':
-"""
+    'description': """
 Para probar instalar tambien "sale" y "procurement_jit_stock"
 """,
     'depends': [
         'mrp',
         'procurement_jit_stock',
     ],
-    'demo_xml': [
-        'mrp_demo.xml',
+    'demo': [
+        # TODO to fix data to pass test
+        # 'mrp_demo.xml',
         ],
     'test': [],
     'data': [
