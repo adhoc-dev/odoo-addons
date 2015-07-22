73181745932e29a4ffff35fe9e5195ab73d92cf2
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-21 23:04:20 -0300

    FIX

--- partner_person/res_partner.py
+++ partner_person/res_partner.py
@@ -124,9 +124,9 @@ class res_partner(models.Model):
         )
     husband_id = fields.Many2one(
         'res.partner',
-        # compute='_get_husband',
-        # inverse='_set_wife',
-        # search='_search_husband',
+        compute='_get_husband',
+        inverse='_set_wife',
+        search='_search_husband',
         string='Husband',
         domain=[('sex', '=', 'M'), ('is_company', '=', False)],
         context={'default_sex': 'M', 'is_person': True}
@@ -138,83 +138,83 @@ class res_partner(models.Model):
         context={'default_sex': 'F', 'is_person': True}
         )
 
-    @api.one
-    @api.onchange('firstname', 'lastname')
-    @api.constrains('firstname', 'lastname')
-    def build_name(self):
-        self.name = '%s %s' % (
-            self.lastname or '', self.firstname or '')
+    # @api.one
+    # @api.onchange('firstname', 'lastname')
+    # @api.constrains('firstname', 'lastname')
+    # def build_name(self):
+    #     self.name = '%s %s' % (
+    #         self.lastname or '', self.firstname or '')
 
-    # def name_get(self, cr, uid, ids, context=None):
-    #     if context is None:
-    #         context = {}
-    #     if isinstance(ids, (int, long)):
-    #         ids = [ids]
-    #     res = []
-    #     for record in self.browse(cr, uid, ids, context=context):
-    #         name = record.name
-    #         national_identity = ''
-    #         if record.national_identity:
-    #             national_identity = '[' + record.national_identity + ']'
-    #         name = "%s %s" % (name, national_identity)
-    #         if record.parent_id and not record.is_company:
-    #             name = "%s, %s" % (record.parent_id.name, name)
-    #         if context.get('show_address'):
-    #             name = name + "\n" + \
-    #                 self._display_address(
-    #                     cr, uid, record, without_company=True, context=context)
-    #             name = name.replace('\n\n', '\n')
-    #             name = name.replace('\n\n', '\n')
-    #         if context.get('show_email') and record.email:
-    #             name = "%s <%s>" % (name, record.email)
-    #         res.append((record.id, name))
-    #     return res
+    def name_get(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        res = []
+        for record in self.browse(cr, uid, ids, context=context):
+            name = record.name
+            national_identity = ''
+            if record.national_identity:
+                national_identity = '[' + record.national_identity + ']'
+            name = "%s %s" % (name, national_identity)
+            if record.parent_id and not record.is_company:
+                name = "%s, %s" % (record.parent_id.name, name)
+            if context.get('show_address'):
+                name = name + "\n" + \
+                    self._display_address(
+                        cr, uid, record, without_company=True, context=context)
+                name = name.replace('\n\n', '\n')
+                name = name.replace('\n\n', '\n')
+            if context.get('show_email') and record.email:
+                name = "%s <%s>" % (name, record.email)
+            res.append((record.id, name))
+        return res
 
-    # def name_search(
-    #         self, cr, uid, name, args=None, operator='ilike',
-    #         context=None, limit=100):
-    #     if not args:
-    #         args = []
-    #     if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
-    #         # search on the name of the contacts and of its company
-    #         search_name = name
-    #         if operator in ('ilike', 'like'):
-    #             search_name = '%%%s%%' % name
-    #         if operator in ('=ilike', '=like'):
-    #             operator = operator[1:]
-    #         query_args = {'name': search_name}
-    #         # TODO: simplify this in trunk with `display_name`, once it is stored
-    #         # Perf note: a CTE expression (WITH ...) seems to have an even higher cost
-    #         #            than this query with duplicated CASE expressions. The bulk of
-    #         #            the cost is the ORDER BY, and it is inevitable if we want
-    #         #            relevant results for the next step, otherwise we'd return
-    #         #            a random selection of `limit` results.
-    #         query = ('''SELECT partner.id FROM res_partner partner
-    #                                       LEFT JOIN res_partner company
-    #                                            ON partner.parent_id = company.id
-    #                     WHERE   partner.national_identity ''' + operator + ''' %(name)s OR 
-    #                             partner.email ''' + operator + ''' %(name)s OR 
-    #                           CASE
-    #                                WHEN company.id IS NULL OR partner.is_company
-    #                                    THEN partner.name
-    #                                ELSE company.name || ', ' || partner.name
-    #                           END ''' + operator + ''' %(name)s
-    #                     ORDER BY
-    #                           CASE
-    #                                WHEN company.id IS NULL OR partner.is_company
-    #                                    THEN partner.name
-    #                                ELSE company.name || ', ' || partner.name
-    #                           END''')
-    #         if limit:
-    #             query += ' limit %(limit)s'
-    #             query_args['limit'] = limit
-    #         cr.execute(query, query_args)
-    #         ids = map(lambda x: x[0], cr.fetchall())
-    #         ids = self.search(
-    #             cr, uid, [('id', 'in', ids)] + args, limit=limit, context=context)
-    #         if ids:
-    #             return self.name_get(cr, uid, ids, context)
-    #     return super(res_partner, self).name_search(cr, uid, name, args, operator=operator, context=context, limit=limit)
+    def name_search(
+            self, cr, uid, name, args=None, operator='ilike',
+            context=None, limit=100):
+        if not args:
+            args = []
+        if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
+            # search on the name of the contacts and of its company
+            search_name = name
+            if operator in ('ilike', 'like'):
+                search_name = '%%%s%%' % name
+            if operator in ('=ilike', '=like'):
+                operator = operator[1:]
+            query_args = {'name': search_name}
+            # TODO: simplify this in trunk with `display_name`, once it is stored
+            # Perf note: a CTE expression (WITH ...) seems to have an even higher cost
+            #            than this query with duplicated CASE expressions. The bulk of
+            #            the cost is the ORDER BY, and it is inevitable if we want
+            #            relevant results for the next step, otherwise we'd return
+            #            a random selection of `limit` results.
+            query = ('''SELECT partner.id FROM res_partner partner
+                                          LEFT JOIN res_partner company
+                                               ON partner.parent_id = company.id
+                        WHERE   partner.national_identity ''' + operator + ''' %(name)s OR 
+                                partner.email ''' + operator + ''' %(name)s OR 
+                              CASE
+                                   WHEN company.id IS NULL OR partner.is_company
+                                       THEN partner.name
+                                   ELSE company.name || ', ' || partner.name
+                              END ''' + operator + ''' %(name)s
+                        ORDER BY
+                              CASE
+                                   WHEN company.id IS NULL OR partner.is_company
+                                       THEN partner.name
+                                   ELSE company.name || ', ' || partner.name
+                              END''')
+            if limit:
+                query += ' limit %(limit)s'
+                query_args['limit'] = limit
+            cr.execute(query, query_args)
+            ids = map(lambda x: x[0], cr.fetchall())
+            ids = self.search(
+                cr, uid, [('id', 'in', ids)] + args, limit=limit, context=context)
+            if ids:
+                return self.name_get(cr, uid, ids, context)
+        return super(res_partner, self).name_search(cr, uid, name, args, operator=operator, context=context, limit=limit)
 
     # Como no anduvo sobre escribiendo con la nueva api, tuvimos que hacerlo con la vieja
     # display_name = fields.Char(
@@ -234,16 +234,16 @@ class res_partner(models.Model):
     #     self.display_name = self.with_context({}).name_get()
 
 
-    # _display_name = lambda self, *args, **kwargs: self._display_name_compute(*args, **kwargs)
+    _display_name = lambda self, *args, **kwargs: self._display_name_compute(*args, **kwargs)
 
-    # _display_name_store_triggers = {
-    #     'res.partner': (lambda self,cr,uid,ids,context=None: self.search(cr, uid, [('id','child_of',ids)], context=dict(active_test=False)),
-    #                     ['parent_id', 'is_company', 'name', 'national_identity'], 10)
-    #     # Se agrega national_identity aqui
-    # }
+    _display_name_store_triggers = {
+        'res.partner': (lambda self,cr,uid,ids,context=None: self.search(cr, uid, [('id','child_of',ids)], context=dict(active_test=False)),
+                        ['parent_id', 'is_company', 'name', 'national_identity'], 10)
+        # Se agrega national_identity aqui
+    }
 
-    # _columns = {
-    #     'display_name': old_fields.function(_display_name, type='char', string='N2222asdasdadsame', store=_display_name_store_triggers, select=True),
-    # }
+    _columns = {
+        'display_name': old_fields.function(_display_name, type='char', string='N2222asdasdadsame', store=_display_name_store_triggers, select=True),
+    }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
