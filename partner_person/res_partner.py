4b27f8b75b6cfe21dd579aef6dc0524261f9abfc
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-21 22:10:18 -0300

    IMP try travis

--- partner_person/res_partner.py
+++ partner_person/res_partner.py
@@ -139,76 +139,76 @@ class res_partner(models.Model):
         self.name = '%s %s' % (
             self.lastname or '', self.firstname or '')
 
-    def name_get(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        res = []
-        for record in self.browse(cr, uid, ids, context=context):
-            name = record.name
-            national_identity = ''
-            if record.national_identity:
-                national_identity = '[' + record.national_identity + ']'
-            name = "%s %s" % (name, national_identity)
-            if record.parent_id and not record.is_company:
-                name = "%s, %s" % (record.parent_id.name, name)
-            if context.get('show_address'):
-                name = name + "\n" + \
-                    self._display_address(
-                        cr, uid, record, without_company=True, context=context)
-                name = name.replace('\n\n', '\n')
-                name = name.replace('\n\n', '\n')
-            if context.get('show_email') and record.email:
-                name = "%s <%s>" % (name, record.email)
-            res.append((record.id, name))
-        return res
+    # def name_get(self, cr, uid, ids, context=None):
+    #     if context is None:
+    #         context = {}
+    #     if isinstance(ids, (int, long)):
+    #         ids = [ids]
+    #     res = []
+    #     for record in self.browse(cr, uid, ids, context=context):
+    #         name = record.name
+    #         national_identity = ''
+    #         if record.national_identity:
+    #             national_identity = '[' + record.national_identity + ']'
+    #         name = "%s %s" % (name, national_identity)
+    #         if record.parent_id and not record.is_company:
+    #             name = "%s, %s" % (record.parent_id.name, name)
+    #         if context.get('show_address'):
+    #             name = name + "\n" + \
+    #                 self._display_address(
+    #                     cr, uid, record, without_company=True, context=context)
+    #             name = name.replace('\n\n', '\n')
+    #             name = name.replace('\n\n', '\n')
+    #         if context.get('show_email') and record.email:
+    #             name = "%s <%s>" % (name, record.email)
+    #         res.append((record.id, name))
+    #     return res
 
-    def name_search(
-            self, cr, uid, name, args=None, operator='ilike',
-            context=None, limit=100):
-        if not args:
-            args = []
-        if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
-            # search on the name of the contacts and of its company
-            search_name = name
-            if operator in ('ilike', 'like'):
-                search_name = '%%%s%%' % name
-            if operator in ('=ilike', '=like'):
-                operator = operator[1:]
-            query_args = {'name': search_name}
-            # TODO: simplify this in trunk with `display_name`, once it is stored
-            # Perf note: a CTE expression (WITH ...) seems to have an even higher cost
-            #            than this query with duplicated CASE expressions. The bulk of
-            #            the cost is the ORDER BY, and it is inevitable if we want
-            #            relevant results for the next step, otherwise we'd return
-            #            a random selection of `limit` results.
-            query = ('''SELECT partner.id FROM res_partner partner
-                                          LEFT JOIN res_partner company
-                                               ON partner.parent_id = company.id
-                        WHERE   partner.national_identity ''' + operator + ''' %(name)s OR 
-                                partner.email ''' + operator + ''' %(name)s OR 
-                              CASE
-                                   WHEN company.id IS NULL OR partner.is_company
-                                       THEN partner.name
-                                   ELSE company.name || ', ' || partner.name
-                              END ''' + operator + ''' %(name)s
-                        ORDER BY
-                              CASE
-                                   WHEN company.id IS NULL OR partner.is_company
-                                       THEN partner.name
-                                   ELSE company.name || ', ' || partner.name
-                              END''')
-            if limit:
-                query += ' limit %(limit)s'
-                query_args['limit'] = limit
-            cr.execute(query, query_args)
-            ids = map(lambda x: x[0], cr.fetchall())
-            ids = self.search(
-                cr, uid, [('id', 'in', ids)] + args, limit=limit, context=context)
-            if ids:
-                return self.name_get(cr, uid, ids, context)
-        return super(res_partner, self).name_search(cr, uid, name, args, operator=operator, context=context, limit=limit)
+    # def name_search(
+    #         self, cr, uid, name, args=None, operator='ilike',
+    #         context=None, limit=100):
+    #     if not args:
+    #         args = []
+    #     if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
+    #         # search on the name of the contacts and of its company
+    #         search_name = name
+    #         if operator in ('ilike', 'like'):
+    #             search_name = '%%%s%%' % name
+    #         if operator in ('=ilike', '=like'):
+    #             operator = operator[1:]
+    #         query_args = {'name': search_name}
+    #         # TODO: simplify this in trunk with `display_name`, once it is stored
+    #         # Perf note: a CTE expression (WITH ...) seems to have an even higher cost
+    #         #            than this query with duplicated CASE expressions. The bulk of
+    #         #            the cost is the ORDER BY, and it is inevitable if we want
+    #         #            relevant results for the next step, otherwise we'd return
+    #         #            a random selection of `limit` results.
+    #         query = ('''SELECT partner.id FROM res_partner partner
+    #                                       LEFT JOIN res_partner company
+    #                                            ON partner.parent_id = company.id
+    #                     WHERE   partner.national_identity ''' + operator + ''' %(name)s OR 
+    #                             partner.email ''' + operator + ''' %(name)s OR 
+    #                           CASE
+    #                                WHEN company.id IS NULL OR partner.is_company
+    #                                    THEN partner.name
+    #                                ELSE company.name || ', ' || partner.name
+    #                           END ''' + operator + ''' %(name)s
+    #                     ORDER BY
+    #                           CASE
+    #                                WHEN company.id IS NULL OR partner.is_company
+    #                                    THEN partner.name
+    #                                ELSE company.name || ', ' || partner.name
+    #                           END''')
+    #         if limit:
+    #             query += ' limit %(limit)s'
+    #             query_args['limit'] = limit
+    #         cr.execute(query, query_args)
+    #         ids = map(lambda x: x[0], cr.fetchall())
+    #         ids = self.search(
+    #             cr, uid, [('id', 'in', ids)] + args, limit=limit, context=context)
+    #         if ids:
+    #             return self.name_get(cr, uid, ids, context)
+    #     return super(res_partner, self).name_search(cr, uid, name, args, operator=operator, context=context, limit=limit)
 
     # Como no anduvo sobre escribiendo con la nueva api, tuvimos que hacerlo con la vieja
     # display_name = fields.Char(
