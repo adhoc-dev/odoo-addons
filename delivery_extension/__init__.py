1e8c222e4da08be80961cd826b57afaf5ac5873a
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-22 00:40:29 -0300

    DEL delivery extension

--- delivery_extension/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-import stock
-# comentamos el import sale para desabiltar la opcion de que se complete el monto
--- delivery_extension/stock.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import models, fields
-
-
-class stock_picking(models.Model):
-    _inherit = 'stock.picking'
-
-    declared_value = fields.Float(
-        'Declared Value', digits=(16, 2),)
