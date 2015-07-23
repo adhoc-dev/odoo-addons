b5ecb9c41aacea5450966a2539dc5a6af56ef168
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-23 10:44:37 -0300

    FIX sale order prod attach prod pack

--- sale_order_mail_product_attach_prod_pack/__init__.py
+++ sale_order_mail_product_attach_prod_pack/__init__.py
@@ -19,7 +19,6 @@
 #
 ##############################################################################
 
-import email_template
 import sale
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
