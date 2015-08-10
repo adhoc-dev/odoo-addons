8495981d6cd265d9a5a7053e0c2988255424f7f0
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-08-10 09:29:59 -0300

    FIX stock-py

--- stock_traceability_report/stock.py
+++ stock_traceability_report/stock.py
@@ -13,7 +13,7 @@ class stock_picking(models.Model):
     def _get_moves(self):
         moves = []
         for quant in self.quant_ids:
-            moves.append(move.id for move in quant.history_ids)
+            moves.append([move.id for move in quant.history_ids])
         self.move_ids = moves
 
     move_ids = fields.One2many('stock.move', compute='_get_moves')
