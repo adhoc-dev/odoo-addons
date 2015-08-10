956e039c8fa2c39ad3c8577ba36bad8018c43a8f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-10 18:44:54 -0300

    DEL account_bank_voucher replaced  by account_statement_move_import

--- account_bank_voucher/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import wizard
-from . import account
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- account_bank_voucher/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import bank_statement_populate
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
