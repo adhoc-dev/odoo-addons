3f9243655a7f61f6cd58da086e62edbcaf932b01
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-30 10:05:15 -0300

    ADD partner_area/ and crm_partner_area

---
+++ crm_partner_area/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CRM Partner Area',
+    'version': '1.0',
+    'category': 'Tools',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+CRM Partner Area
+===========================
+
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'partner_area',
+        'crm',
+    ],
+    'data': [
+        'crm_area_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
