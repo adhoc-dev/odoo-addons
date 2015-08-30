40f54345d4ac255022620a7da5ebb6474747fc36
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-29 22:26:01 -0300

    add crm_phonecall_extend/

---
+++ crm_phonecall_extend/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{'author': 'Ingenieria ADHOC',
+    'demo_xml': [
+    ],
+    'depends': [
+        'crm_phonecall_type'
+        ],
+    'description': 'CRM Phonecalls Extension',
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'CRM Phonecalls Extension',
+    'test': [],
+    'data': [
+        'crm_lead_view.xml',
+    ],
+    'version': 'No version',
+    'auto_install': False,
+    'category': '',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
