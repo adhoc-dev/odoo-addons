5bb70bf4fef1841f128882732cce01bd3bc2b496
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-29 22:23:53 -0300

    ADD crm_phonecall_type

---
+++ crm_phonecall_type/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{'author': 'Ingenieria ADHOC',
+    'demo_xml': [
+    ],
+    'depends': [
+        'crm'
+        ],
+    'description': 'CRM Phonecall Type',
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'CRM Phonecall Type',
+    'test': [],
+    'data': [
+        # 'wizard/crm_opportunity_to_phonecall_view.xml',
+        'wizard/crm_phonecall_to_phonecall_view.xml',
+        'crm_phonecall_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'version': 'No version',
+    'auto_install': False,
+    'category': '',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
