1c1262bf15465e3dd6097ccf01c6e963df7a1273
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-09-02 19:46:25 -0300

    IMP check module version

--- account_check/__openerp__.py
+++ account_check/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account Check Management',
-    'version': '8.0.2.1.0',
+    'version': '8.0.2.1.1',
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Accounting, Payment, Check, Third, Issue',
