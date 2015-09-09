5bdcd93e34a886a28f1d628269ba7a17b67a6a77
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-09-09 11:41:18 -0300

    FIX reference in invoices

--- sale_multiple_invoice/wizard/sale_make_invoice_advance.py
+++ sale_multiple_invoice/wizard/sale_make_invoice_advance.py
@@ -74,6 +74,7 @@ class sale_advance_payment_inv(osv.osv_memory):
                     _('This sale order has already some invoices created!'))
 
             inv_ids = []
+            sale_ref = sale_obj.browse(cr, uid, sale_ids[0], context=context).client_order_ref
             for invoice_count in range(wizard.invoice_qty - 1):
                 for sale_id, inv_values in self._prepare_advance_invoice_vals(cr, uid, ids, context=context):
                     first_invoice_date = datetime.strptime(
@@ -82,8 +83,9 @@ class sale_advance_payment_inv(osv.osv_memory):
                         months=invoice_count)).strftime(DEFAULT_SERVER_DATE_FORMAT)
                     invoice_id = self._create_invoices(
                         cr, uid, inv_values, sale_id, context=context)
+                    reference = _(sale_ref + ' Invoice ' + str(invoice_count + 1) + ' of ' + str(wizard.invoice_qty))
                     invoice_obj.write(
-                        cr, uid, invoice_id, {'date_invoice': invoice_date}, context=context)
+                        cr, uid, invoice_id, {'date_invoice': invoice_date, 'reference': reference}, context=context)
                     inv_ids.append(invoice_id)
 
             # create the final invoices of the active sales orders
@@ -91,8 +93,9 @@ class sale_advance_payment_inv(osv.osv_memory):
                 months=wizard.invoice_qty - 1)).strftime(DEFAULT_SERVER_DATE_FORMAT)
             invoice_id = sale_obj.manual_invoice(
                 cr, uid, sale_ids, context).get('res_id', False)
+            reference = _(sale_ref + ' Invoice ' + str(wizard.invoice_qty) + ' of ' + str(wizard.invoice_qty))
             invoice_obj.write(
-                cr, uid, invoice_id, {'date_invoice': invoice_date}, context=context)
+                cr, uid, invoice_id, {'date_invoice': invoice_date,  'reference': reference}, context=context)
 
             if context.get('open_invoices', False):
                 return sale_obj.action_view_invoice(cr, uid, sale_ids, context=context)
