5224866d980a367ff2a6d65bd5ef9ef670082994
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-09-29 11:08:40 -0300

    ADD sale_exception_price_security

---
+++ sale_exception_price_security/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Exception - Price Security Integration',
+    'version': '8.0.0.0.0',
+    'description': """
+Sale Exception - Price Security Integration
+===========================================
+When using together price_security and sale_exceptions, it changes the
+constraint for the user that can not save a sales order with discounts greater
+than allowed to the sale exception constraint that don't allows user to confirm
+sale order
+""",
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'depends': [
+        'sale_exceptions',
+        'price_security',
+        ],
+    'data': [
+        'data/data.xml',
+        ],
+    'demo': [
+        ],
+    'test': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
