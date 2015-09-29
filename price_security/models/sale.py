f640b0b747eb4f35d3163387919e6aa7675df9e8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-09-29 11:07:40 -0300

    IMP price_security for sale_exception_price_security compatibility

--- price_security/models/sale.py
+++ price_security/models/sale.py
@@ -13,20 +13,15 @@ class sale_order_line(models.Model):
         related='product_id.can_modify_prices',
         readonly=True,
         string='Product Can modify prices')
-    # agregamos este campo porque si hacemos readonly el campo tax_id el impuesto puede ser sobre escritor por la funcion create de sale order line. de esta manera, oculamos tax para que escriba y ponemos esta copia readonly
-    # tampoco nos anduvo porque no podiamos hacerlo visible segun el grupo
-    # tax_id_copy = fields.Many2many(
-    #     'account.tax',
-    #     string='Taxes',
-    #     related='tax_id',
-    #     readonly=True
-    #     )
 
     @api.one
     @api.constrains(
         'discount', 'product_can_modify_prices')
     def check_discount(self):
-        if self.user_has_groups('price_security.group_restrict_prices') and not self.product_can_modify_prices:
+        if (
+                self.user_has_groups('price_security.group_restrict_prices')
+                and not self.product_can_modify_prices
+                ):
             self.env.user.check_discount(
                 self.discount,
                 self.order_id.pricelist_id.id)
