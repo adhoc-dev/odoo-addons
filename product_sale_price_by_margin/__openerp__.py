13ee49c5ddf49405d6d6f7a7e0d3fcb4522b591b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-10-15 17:30:55 -0300

    IMP increase version and delete wrong translations

--- product_sale_price_by_margin/__openerp__.py
+++ product_sale_price_by_margin/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Product Sale Price By Margin',
-    'version': '8.0.0.1.0',
+    'version': '8.0.0.2.0',
     'category': 'Product',
     'sequence': 14,
     'summary': '',
