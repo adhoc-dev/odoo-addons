eecc844bfd5dc6c28bf77ed5aaf6c9058301cb6a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-12-29 18:25:21 -0300

    ADD las date update for replensiment cost

--- product_replenishment_cost_currency/__openerp__.py
+++ product_replenishment_cost_currency/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Cost Currency",
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.1.0',
     'category': 'Product',
     'sequence': 14,
     'author':  'ADHOC SA',
