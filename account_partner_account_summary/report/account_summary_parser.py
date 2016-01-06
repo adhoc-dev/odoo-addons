b9853e26e81bc1cfc76481c2d7c0465c07a3c2d8
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-01-06 10:12:55 -0300

    FIX reverse some changes

--- account_partner_account_summary/report/account_summary_parser.py
+++ account_partner_account_summary/report/account_summary_parser.py
@@ -175,8 +175,6 @@ class Parser(rml_parse):
         for it_move in moves:
             accumulated_balance += self.get_move_debit(
                 it_move) - self.get_move_credit(it_move)
-            # print 'get_move_credit', self.get_move_credit(it_move)
-            # print 'accumulated_balance', accumulated_balance
             if it_move == move:
                 break
         return accumulated_balance
@@ -200,7 +198,7 @@ class Parser(rml_parse):
         return lines
 
     def get_move_line_name(self, line):
-        return line.display_name
+        return line.name
 
     def get_move_line_debit_to_print(self, line):
         if line.debit == 0.0:
@@ -335,17 +333,17 @@ class Parser(rml_parse):
             return res[0][0]
 
     def get_initial_balance(self, partner, context=None):
-        return self.get_initial_debit(partner) - self.get_initial_credit(partner)
+        return self.get_initial_debit(partner, context) - self.get_initial_credit(partner, context)
 
     def get_initial_credit_to_print(self, partner, context=None):
-        initial_credit = self.get_initial_credit(partner)
+        initial_credit = self.get_initial_credit(partner, context)
         if not initial_credit:
             return 0
         else:
             return initial_credit
 
     def get_initial_debit_to_print(self, partner, context=None):
-        initial_debit = self.get_initial_debit(partner)
+        initial_debit = self.get_initial_debit(partner, context)
         if not initial_debit:
             return 0
         else:
