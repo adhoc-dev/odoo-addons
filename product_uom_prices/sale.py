f8d4c558c4eb61544cfeb19810eeb4e4a5552ad8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-01-11 17:28:00 -0300

    FIX

--- product_uom_prices/sale.py
+++ product_uom_prices/sale.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields, api
+from openerp import models, fields, api, SUPERUSER_ID
 
 
 class sale_order_line(models.Model):
@@ -46,15 +46,36 @@ class sale_order_line(models.Model):
                     not uom and product.use_uom_prices and
                     self.env.user.company_id.default_uom_prices
                     ):
+                uom_id = product.uom_price_ids[0].uom_id.id
                 res['value'].update(
-                    {'product_uom': product.uom_price_ids[0].uom_id.id})
-                res2 = super(sale_order_line, self).product_id_change(
-                    pricelist, product, qty=qty, uom=uom,
-                    qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
-                    lang=lang, update_tax=update_tax, date_order=date_order,
-                    packaging=packaging, fiscal_position=fiscal_position,
-                    flag=flag)
-                print 'res22 uom prices', res2
+                    {'product_uom': uom_id})
+                # res2 = super(sale_order_line, self).product_id_change(
+                #     pricelist, product.id, qty=qty, uom=uom,
+                #     qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+                #     lang=lang, update_tax=update_tax, date_order=date_order,
+                #     packaging=packaging, fiscal_position=fiscal_position,
+                #     flag=flag)
+                # si tenemos instalado el modulo "sale_stock", entonces odoo por defecto
+                # hace un hack y borrar la uom elegida y a uom y precio por defecto del producto
+                # en nuestro caso entonces termina quedando mal porque cambiamos la uom pero no el precio
+                # por eso forzamos el recalculo del precio
+                price = self.env['product.pricelist'].browse(
+                    pricelist).with_context(
+                        uom=uom_id, date=date_order,).price_get(
+                        product.id, qty or 1.0, partner_id)[pricelist]
+                if price:
+                    if self.env.uid == SUPERUSER_ID and self._context.get(
+                            'company_id'):
+                        taxes = product.taxes_id.filtered(
+                            lambda r: r.company_id.id == self._context[
+                            'company_id'])
+                    else:
+                        taxes = product.taxes_id
+                    price = self.env['account.tax']._fix_tax_included_price(
+                        price, taxes, result['tax_id'])
+                    res['value'].update({'price_unit': price})
+                print 'price', price
+                # print 'res22 uom prices', res2
             # we do this because odoo overwrite view domain
             if 'domain' not in res:
                 res['domain'] = {}
