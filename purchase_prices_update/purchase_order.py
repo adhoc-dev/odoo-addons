b9b32f1798b2defbad0cfc500d9f4db458fc2858
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-01-19 11:56:06 -0300

    FIX purchas price update

--- purchase_prices_update/purchase_order.py
+++ purchase_prices_update/purchase_order.py
@@ -12,12 +12,11 @@ class purchase_order(models.Model):
     @api.one
     def update_prices(self):
         for line in self.order_line:
-            price = self.pricelist_id.price_get(
-                line.product_id.id,
-                line.product_qty or 1.0,
-                self.partner_id.id, context={
-                    'uom': line.product_uom.id,
-                    'date': self.date_order,
-                })[self.pricelist_id.id]
-            line.write({'price_unit': price})
+            price = self.pricelist_id.with_context(
+                uom=line.product_uom.id,
+                date=self.date_order).price_get(
+                    line.product_id.id,
+                    line.product_qty or 1.0,
+                    self.partner_id.id)[self.pricelist_id.id]
+            line.price_unit = price
         return True
